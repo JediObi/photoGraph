3d8fcc4c78566a1fee491c013d6f00ee15138cd5
fc <1828390908@qq.com>
2017-09-22 03:19:28 -0700

    nothing

--- src/App.js
+++ src/App.js
@@ -1,41 +1,14 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
 //import './App.css';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import MyAwesomeReactComponent from './MyAwesomeReactComponent';
-
-
 import CardOnlyMedia from './CardOnlyMedia';
 
-
-/*
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
-*/
-
-//把文件名转换成文件路径
-/*
-
-*/
 class App extends Component {
     render() {
         return (
-            <MuiThemeProvider>
-                {/*<CardOnlyMedia/>*/}
-            </MuiThemeProvider>
+            <CardOnlyMedia>
+
+            </CardOnlyMedia>
         );
     }
 }
--- src/CardOnlyMedia.js
+++ src/CardOnlyMedia.js
@@ -1,19 +1,7 @@
 import React, {Component} from 'react';
-import {Card, CardMedia, CardTitle} from 'material-ui/Card';
 import imageDatas from './imageDatas.json';
-/*
-class CardOnlyMedia extends Component{
-    render(){
-        return (
-            <Card>
-                <CardMedia overlay={<CardTitle title={"overlay title"} subtitle={"overlay subtitle"}></CardTitle>}>
-                    <img src={require('./images/1.jpg')} alt=""></img>
-                </CardMedia>
-            </Card>
-        );
-    }
-}
-*/
+import './CardOnlyMedia.css';
+
 function genImageURL(imageDataArr) {
     for (let i = 0, j = imageDatas.length; i < j; i++) {
         let singleImageData = imageDataArr[i];
@@ -26,41 +14,134 @@ function genImageURL(imageDataArr) {
 let imageDataArr = genImageURL(imageDatas);
 //alert(imageDataArr[0].imageURL);
 
-
-class ImageFigure extends Component{
-    render(){
-        return(
-            <figure>
-                <img/>
-                <figcaption>
-                    <h2></h2>
-                </figcaption>
-            </figure>
-        );
-    }
+const ImageFigure = (props) => {
+    return (
+        <figure className="img-figure">
+            <img src={props.data.imageURL} alt={props.data.title}/>
+            <figcaption>
+                <h2 className="img-title">{props.data.title}</h2>
+            </figcaption>
+        </figure>
+    );
 }
 
-class CardOnlyMedia extends Component{
-    render(){
+class CardOnlyMedia extends Component {
+
+    let
+    Constant = {
+        centerPos: {
+            left: 0,
+            top: 0
+        },
+        hPosRange: {
+            leftSecX: [0, 0],
+            rightSecX: [0, 0],
+            y: [0, 0]
+        },
+        vPosRange: {
+            x: [0, 0],
+            topY: [0, 0]
+        }
+    };
+
+    constructor() {
+        super();
+        this.state = {
+            /* imgsArrageArr:[{
+                 pos:{
+                     left: '0',
+                     top: '0'
+                 }
+             }]*/
+        }
+    }
+
+    componentDidMount() {
+        let stageDOM = React.findDOMNode(this.refs.stage);
+        let stageW = stageDOM.scrollWidth,
+            stageH = stageDOM.scrollHeight;
+        let halfStageW = Math.ceil(stageW / 2),
+            halfStageH = Math.ceil(stageH / 2);
+
+        let imgFigureDOM = React.findDOMNode(this.refs.imageFigure0);
+        let imgW = imgFigureDOM.scrollWidth,
+            imgH = imgFigureDOM.scrollHeight;
+        let halfImgW = Math.ceil(imgW / 2),
+            halfImgH = Math.ceil(imgH / 2);
+
+        //计算中心图片的坐标
+        this.Constant.centerPos = {
+            left: halfStageW - halfImgW,
+            top: halfStageH - halfImgH
+        };
+        //计算水平方向的坐标范围
+        this.Constant.hPosRange.leftSecX[0] = -halfImgW;
+        this.Constant.hPosRange.leftSecX[1] = halfStageW - halfImgW * 3;
+        this.Constant.hPosRange.rightSecX[0] = stageW + halfImgW;
+        this.Constant.hPosRange.rightSecX[1] = stageW - halfImgW;
+        this.Constant.hPosRange.y[0] = -halfImgH;
+        this.Constant.hPosRange.y[1] = stageH - halfImgH;
+
+        //计算上部坐标范围
+        this.Constant.vPosRange.topY[0] = -halfImgH;
+        this.Constant.vPosRange.topY[1] = halfStageH - halfImgH * 3;
+        this.Constant.vPosRange.x[0] = halfStageW - imgW;
+        this.Constant.vPosRange.x[1] = halfStageW;
+
+        this.reArrange(0);
+
+    }
+
+    reArrange(centerIndex) {
+        //获取状态中的图片坐标数组，获取坐标分布范围
+        let imgsArrangeArr = this.state.imgsArrangeArr,
+            Constant = this.Constant;
+        //从坐标分布范围中取出具体的坐标块
+        let centerPos = Constant.centerPos,
+            hPosRange = Constant.hPosRange,
+            vPosRange = Constant.vPosRange;
+        //从坐标块中取出具体的边界值
+        let hPosRangeLeftSecX = hPosRange.leftSecX,
+            hPosRangeRightSecX = hPosRange.rightSecX,
+            hPosRangeY = hPosRange.y,
+            vPosRangeTopY = vPosRange.topY,
+            vPosRangeX = vPosRange.x;
+        //从
+         let   imgsArrangeTopArr = [],
+            topImageNumber = Math.ceil(Math.random() * 2),
+            tomImgSpliceIndex = 0,
+            imgsArrangeCenterArr = imgsArrangeArr.splice()
+    }
+
+    render() {
 
         let controllerUnits = [],
             imgFigures = [];
 
-        imageDataArr.forEach((value)=>{
-            imgFigures.push(<ImageFigure data={value}/>);
-        });
+        imageDataArr.forEach(function (value, index) {
+            if (!this.state.imgsArrangeArr[index]) {
+                this.state.imgsArrangeArr[index] = {
+                    pos: {
+                        left: 0,
+                        top: 0
+                    }
+                }
+            }
+            imgFigures.push(<ImageFigure data={value} ref={'imgFigure' + index}/>);
+        }.bind(this));
 
-        return(
-            <section classname="stage">
+        return (
+            <section className="stage" ref="stage">
                 <section className="img-sec">
                     {imgFigures}
                 </section>
-                <nav className="controller-nav"></nav>
+                <nav className="controller-nav">
+                    {controllerUnits}
+                </nav>
             </section>
         );
     }
 }
-
 
 
 export default CardOnlyMedia;
